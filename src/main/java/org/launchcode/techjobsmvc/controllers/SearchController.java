5429fa957035742334d6618e24b0ac4715ca9175
JohnProphet <johnldudash@gmail.com>
2024-01-28 18:30:29 -0600

    Another Step...

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -22,6 +22,9 @@ public class SearchController {
     }
 
     // TODO #3 - Create a handler to process a search request and render the updated search view.
-
+//    @GetMapping
+//    public String displaySearchResults(Model model SearchType searchType SearchTerm searchTerm) {
+//
+//    }
 }
 
